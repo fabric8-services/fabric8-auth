ea889a184c04268f240079f4474c8a1a8d3af737
Alexey Kazakov <alkazako@redhat.com>
2017-07-13 15:24:25 -0700

    Forward X-Request-ID to tenant service

--- account/init_tenant.go
+++ account/init_tenant.go
@@ -44,7 +44,7 @@ func InitTenant(ctx context.Context, config tenantConfig) error {
 	c.SetJWTSigner(goasupport.NewForwardSigner(ctx))
 
 	// Ignore response for now
-	_, err = c.SetupTenant(ctx, tenant.SetupTenantPath())
+	_, err = c.SetupTenant(goasupport.ForwardContextRequestID(ctx), tenant.SetupTenantPath())
 	if err != nil {
 		return err
 	}
@@ -65,7 +65,7 @@ func UpdateTenant(ctx context.Context, config tenantConfig) error {
 	c.SetJWTSigner(goasupport.NewForwardSigner(ctx))
 
 	// Ignore response for now
-	_, err = c.UpdateTenant(ctx, tenant.SetupTenantPath())
+	_, err = c.UpdateTenant(goasupport.ForwardContextRequestID(ctx), tenant.SetupTenantPath())
 	if err != nil {
 		return err
 	}
