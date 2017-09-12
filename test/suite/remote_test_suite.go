95d73cae149e15d78e6f056a7422f0c8c7952c31
Alexey Kazakov <alkazako@redhat.com>
2017-09-11 19:24:22 -0700

    Auth Service Account

--- test/suite/remote_test_suite.go
+++ test/suite/remote_test_suite.go
@@ -1,41 +1,37 @@
-package gormtestsupport
+package suite
 
 import (
-	config "github.com/fabric8-services/fabric8-auth/configuration"
+	"github.com/fabric8-services/fabric8-auth/configuration"
 	"github.com/fabric8-services/fabric8-auth/log"
 	"github.com/fabric8-services/fabric8-auth/resource"
 
 	"github.com/stretchr/testify/suite"
 )
 
-var _ suite.SetupAllSuite = &RemoteTestSuite{}
-var _ suite.TearDownAllSuite = &RemoteTestSuite{}
-
 // NewRemoteTestSuite instanciate a new RemoteTestSuite
-func NewRemoteTestSuite(configFilePath string) RemoteTestSuite {
-	return RemoteTestSuite{configFile: configFilePath}
+func NewRemoteTestSuite() RemoteTestSuite {
+	return RemoteTestSuite{}
 }
 
 // RemoteTestSuite is a base for tests using a gorm Remote
 type RemoteTestSuite struct {
 	suite.Suite
-	configFile    string
-	Configuration *config.ConfigurationData
+	Config *configuration.ConfigurationData
 }
 
 // SetupSuite implements suite.SetupAllSuite
 func (s *RemoteTestSuite) SetupSuite() {
 	resource.Require(s.T(), resource.Remote)
-	configuration, err := config.NewConfigurationData(s.configFile)
+	config, err := configuration.GetConfigurationData()
 	if err != nil {
 		log.Panic(nil, map[string]interface{}{
 			"err": err,
 		}, "failed to setup the configuration")
 	}
-	s.Configuration = configuration
+	s.Config = config
 }
 
 // TearDownSuite implements suite.TearDownAllSuite
 func (s *RemoteTestSuite) TearDownSuite() {
-	s.Configuration = nil // Summon the GC!
+	s.Config = nil // Summon the GC!
 }
