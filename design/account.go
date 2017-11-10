24929ff7e472058556a86f06d3db7c9ae886dc24
shoubhik bose <sbose78@gmail.com>
2017-11-10 15:33:32 +0530

    update description of api

--- design/account.go
+++ design/account.go
@@ -170,7 +170,7 @@ var userDataAttributes = a.Type("UserDataAttributes", func() {
 	a.Attribute("url", d.String, "The url")
 	a.Attribute("company", d.String, "The company")
 	a.Attribute("providerType", d.String, "The IDP provided this identity")
-	a.Attribute("cluster", d.String, "The OpenShift API URL of the cluster where the user's tenant services are being run")
+	a.Attribute("cluster", d.String, "The OpenShift API URL of the cluster where the user is provisioned to")
 	a.Attribute("contextInformation", a.HashOf(d.String, d.Any), "User context information of any type as a json", func() {
 		a.Example(map[string]interface{}{"last_visited_url": "https://a.openshift.io", "space": "3d6dab8d-f204-42e8-ab29-cdb1c93130ad"})
 	})
