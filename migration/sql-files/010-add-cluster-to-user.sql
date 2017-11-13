b7860026c812f3aa0bcfc22b4289301f3ee4285a
shoubhik bose <sbose78@gmail.com>
2017-11-13 12:15:57 +0530

    make sql compatible for old postgres

--- migration/sql-files/010-add-cluster-to-user.sql
+++ migration/sql-files/010-add-cluster-to-user.sql
@@ -1,3 +1,3 @@
-ALTER TABLE users ADD COLUMN IF NOT EXISTS cluster TEXT;
+ALTER TABLE users ADD COLUMN cluster TEXT;
 UPDATE users SET cluster = '{{ index . 0}}'  WHERE cluster is null or cluster = '';
 ALTER TABLE users ALTER COLUMN cluster set NOT NULL ,ADD CHECK (cluster <> '');
