7c096653afb69415af9e1ad559255cddbfba6e04
shoubhik bose <sbose78@gmail.com>
2018-01-02 11:26:38 +0530

    set not null

--- migration/sql-files/012-hide-email.sql
+++ migration/sql-files/012-hide-email.sql
@@ -1,3 +1,3 @@
--- Store identity provider username for the external token
 ALTER TABLE users ADD COLUMN email_private boolean;
 UPDATE USERS SET email_private = false;
+ALTER TABLE USERS ALTER COLUMN email_private set NOT NULL;
