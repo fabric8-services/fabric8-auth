990775e3232a612d4ae66d74889b7da8e5450903
Kishan Sagathiya <kishansagathiya@gmail.com>
2018-01-23 17:08:48 +0530

    Added tests, removed comments and unnecessary variables

--- controller/openid_configuration.go
+++ controller/openid_configuration.go
@@ -1,15 +1,8 @@
 package controller
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
-
 	"github.com/fabric8-services/fabric8-auth/app"
 	"github.com/fabric8-services/fabric8-auth/client"
-	"github.com/fabric8-services/fabric8-auth/errors"
-	"github.com/fabric8-services/fabric8-auth/jsonapi"
-	"github.com/fabric8-services/fabric8-auth/log"
 	"github.com/fabric8-services/fabric8-auth/rest"
 	"github.com/goadesign/goa"
 )
@@ -27,24 +20,6 @@ func NewOpenidConfigurationController(service *goa.Service, configuration LoginC
 
 // Show runs the show action.
 func (c *OpenidConfigurationController) Show(ctx *app.ShowOpenidConfigurationContext) error {
-	keycloakOpenIDConfigurationEndpoint := c.configuration.GetKeycloakURL() + "/auth/realms/" + c.configuration.GetKeycloakRealm() + "/.well-known/openid-configuration"
-	response, err := http.Get(keycloakOpenIDConfigurationEndpoint)
-	if err != nil {
-		log.Error(ctx, map[string]interface{}{}, "request to achieve openid-configuration of keycloak failed")
-		return jsonapi.JSONErrorResponse(ctx, errors.NewInternalError(ctx, err))
-	}
-
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		panic(err.Error())
-	}
-	keycloakOpenIDConfiguration := &app.OpenIDConfiguration{}
-	err = json.Unmarshal(body, keycloakOpenIDConfiguration)
-	if err != nil {
-		log.Error(ctx, map[string]interface{}{}, "unable to unmashal to json")
-		return jsonapi.JSONErrorResponse(ctx, errors.NewInternalError(ctx, err))
-	}
-
 	issuer := rest.AbsoluteURL(ctx.RequestData, "")
 	authorizationEndpoint := rest.AbsoluteURL(ctx.RequestData, client.AuthorizeAuthorizePath())
 	tokenEndpoint := rest.AbsoluteURL(ctx.RequestData, client.ExchangeTokenPath())
@@ -52,18 +27,13 @@ func (c *OpenidConfigurationController) Show(ctx *app.ShowOpenidConfigurationCon
 	jwksURI := c.configuration.GetKeycloakEndpointCerts()
 
 	authOpenIDConfiguration := &app.OpenIDConfiguration{
-		Issuer:                &issuer,
-		AuthorizationEndpoint: &authorizationEndpoint,
-		TokenEndpoint:         &tokenEndpoint,
-		// Our UserinfoEndpoint is not OAuth2.0 compliant. http://openid.net/specs/openid-connect-core-1_0.html#UserInfo
-		// UserinfoEndpoint:
-		EndSessionEndpoint: &logoutEndpoint,
-		// CheckSessionIframe is not supported yet
-		// RegistrationEndpoint is not supported yet
+		Issuer:                 &issuer,
+		AuthorizationEndpoint:  &authorizationEndpoint,
+		TokenEndpoint:          &tokenEndpoint,
+		EndSessionEndpoint:     &logoutEndpoint,
 		ResponseTypesSupported: []string{"code"},
 		JwksURI:                &jwksURI,
-		// AcrValuesSupported OPTIONAL. JSON array containing a list of the Authentication Context Class References that this OP supports.
-		GrantTypesSupported: []string{"authorization_code", "refresh_token", "client_credentials"},
+		GrantTypesSupported:    []string{"authorization_code", "refresh_token", "client_credentials"},
 		// subject_types_supported REQUIRED. JSON array containing a list of the Subject Identifier types that this OP supports. Valid types include pairwise and public.
 		// A Subject Identifier is a locally unique and never reassigned identifier within the Issuer for the End-User, which is intended to be consumed by the Client.
 		// http://openid.net/specs/openid-connect-core-1_0.html#SubjectIDTypes
---
+++ controller/openid_configuration_blackbox_test.go
@@ -0,0 +1,95 @@
+package controller_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/fabric8-services/fabric8-auth/app"
+	"github.com/fabric8-services/fabric8-auth/app/test"
+	"github.com/fabric8-services/fabric8-auth/client"
+	config "github.com/fabric8-services/fabric8-auth/configuration"
+	. "github.com/fabric8-services/fabric8-auth/controller"
+	"github.com/fabric8-services/fabric8-auth/resource"
+	"github.com/fabric8-services/fabric8-auth/rest"
+
+	"github.com/goadesign/goa"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type TestOpenIDConfigurationREST struct {
+	suite.Suite
+	configuration *config.ConfigurationData
+}
+
+func TestRunOpenIDConfigurationREST(t *testing.T) {
+	resource.Require(t, resource.UnitTest)
+	configuration, err := config.GetConfigurationData()
+	if err != nil {
+		log.Panic(nil, map[string]interface{}{
+			"err": err,
+		}, "failed to setup the configuration")
+	}
+	suite.Run(t, &TestOpenIDConfigurationREST{configuration: configuration})
+}
+
+func (s *TestOpenIDConfigurationREST) UnSecuredController() (*goa.Service, *OpenidConfigurationController) {
+	svc := goa.New("Status-Service")
+	return svc, NewOpenidConfigurationController(svc, s.configuration)
+}
+
+func (s *TestOpenIDConfigurationREST) TestShowOpenIDConfiguration() {
+	t := s.T()
+	svc, ctrl := s.UnSecuredController()
+
+	_, openIDConfiguration := test.ShowOpenidConfigurationOK(t, svc.Context, svc, ctrl)
+
+	u := &url.URL{
+		Path: fmt.Sprintf(client.ShowOpenidConfigurationPath()),
+	}
+	prms := url.Values{}
+	req, err := http.NewRequest("GET", u.String(), nil)
+
+	ctx := context.Background()
+	rw := httptest.NewRecorder()
+	goaCtx := goa.NewContext(goa.WithAction(ctx, "OpenIDConfigurationTest"), rw, req, prms)
+	openIDConfigurationCtx, err := app.NewShowOpenidConfigurationContext(goaCtx, req, goa.New("LoginService"))
+	require.Nil(t, err)
+	/*
+		_, openIDConfiguration := test.ShowOpenidConfigurationOK(t, svc.Context, svc, ctrl)*/
+	// err = ctrl.Show(openIDConfigurationCtx)
+	// require.Nil(t, err)
+	/*
+		body, err := ioutil.ReadAll(rw.Body)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		authOpenIDConfiguration := &app.OpenIDConfiguration{}
+		err = json.Unmarshal(body, authOpenIDConfiguration)
+	*/
+	issuer := rest.AbsoluteURL(openIDConfigurationCtx.RequestData, "")
+	authorizationEndpoint := rest.AbsoluteURL(openIDConfigurationCtx.RequestData, client.AuthorizeAuthorizePath())
+	tokenEndpoint := rest.AbsoluteURL(openIDConfigurationCtx.RequestData, client.ExchangeTokenPath())
+	logoutEndpoint := rest.AbsoluteURL(openIDConfigurationCtx.RequestData, client.LogoutLogoutPath())
+	jwksURI := s.configuration.GetKeycloakEndpointCerts()
+
+	expectedOpenIDConfiguration := &app.OpenIDConfiguration{
+		Issuer:                           &issuer,
+		AuthorizationEndpoint:            &authorizationEndpoint,
+		TokenEndpoint:                    &tokenEndpoint,
+		EndSessionEndpoint:               &logoutEndpoint,
+		ResponseTypesSupported:           []string{"code"},
+		JwksURI:                          &jwksURI,
+		GrantTypesSupported:              []string{"authorization_code", "refresh_token", "client_credentials"},
+		SubjectTypesSupported:            []string{},
+		IDTokenSigningAlgValuesSupported: []string{"RS256"},
+	}
+
+	require.Equal(t, openIDConfiguration, expectedOpenIDConfiguration)
+}
