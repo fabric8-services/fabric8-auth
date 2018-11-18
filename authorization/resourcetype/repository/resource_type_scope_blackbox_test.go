7d73dc2767478c607613d09c3be3d08040adee07
Shane Bryzak <sbryzak@gmail.com>
2018-11-19 09:22:13 +1000

    ISSUE-646 added tests for resource type scope
    Signed-off-by: Shane Bryzak <sbryzak@gmail.com>

--- authorization/resourcetype/repository/resource_type_scope_blackbox_test.go
+++ authorization/resourcetype/repository/resource_type_scope_blackbox_test.go
@@ -29,6 +29,92 @@ func (s *resourceTypeScopeBlackBoxTest) SetupTest() {
 	s.resourceTypeRepo = resourcetype.NewResourceTypeRepository(s.DB)
 }
 
+func (s *resourceTypeScopeBlackBoxTest) TestExistsResourceTypeScope() {
+	rt := s.Graph.CreateResourceType()
+
+	rts := &resourcetype.ResourceTypeScope{
+		ResourceTypeID: rt.ResourceType().ResourceTypeID,
+		Name: "foo",
+	}
+
+	err := s.repo.Create(s.Ctx, rts)
+	require.NoError(s.T(), err)
+
+	// Confirm that the scope exists
+	exists, err := s.repo.CheckExists(s.Ctx, rts.ResourceTypeScopeID.String())
+	require.NoError(s.T(), err)
+	require.True(s.T(), exists)
+
+	// Confirm that an invalid scope ID doesn't exist
+	exists, err = s.repo.CheckExists(s.Ctx, uuid.NewV4().String())
+	require.Error(s.T(), err)
+	require.IsType(s.T(), err, errors.NotFoundError{})
+}
+
+func (s *resourceTypeScopeBlackBoxTest) TestCreateFailsForInvalidResourceType() {
+	rts := &resourcetype.ResourceTypeScope{
+		ResourceTypeID: uuid.NewV4(),
+		Name: "foo",
+	}
+
+	err := s.repo.Create(s.Ctx, rts)
+	require.Error(s.T(), err)
+}
+
+func (s *resourceTypeScopeBlackBoxTest) TestSaveOK() {
+	rt := s.Graph.CreateResourceType()
+
+	rts := &resourcetype.ResourceTypeScope{
+		ResourceTypeID: rt.ResourceType().ResourceTypeID,
+		Name: "foo",
+	}
+
+	err := s.repo.Create(s.Ctx, rts)
+	require.NoError(s.T(), err)
+
+	// Change the name and save it
+	rts.Name = "bar"
+	err = s.repo.Save(s.Ctx, rts)
+	require.NoError(s.T(), err)
+
+	// Reload the scope, and confirm the name is updated
+	reloaded, err := s.repo.Load(s.Ctx, rts.ResourceTypeScopeID)
+	require.NoError(s.T(), err)
+	require.Equal(s.T(), reloaded.Name, "bar")
+}
+
+func (s *resourceTypeScopeBlackBoxTest) TestSaveNonExistentScopeFails() {
+	rt := s.Graph.CreateResourceType()
+
+	rts := &resourcetype.ResourceTypeScope{
+		ResourceTypeID: rt.ResourceType().ResourceTypeID,
+		Name: "foo",
+	}
+
+	err := s.repo.Save(s.Ctx, rts)
+	require.Error(s.T(), err)
+}
+
+func (s *resourceTypeScopeBlackBoxTest) TestListOK() {
+	rt := s.Graph.CreateResourceType()
+	rt.AddScope("foo")
+
+	vals, err := s.repo.List(s.Ctx, rt.ResourceType())
+	require.NoError(s.T(), err)
+
+	require.NotNil(s.T(), vals)
+	require.Len(s.T(), vals, 1)
+	require.Equal(s.T(), vals[0].Name, "foo")
+
+	rt.AddScope("bar")
+
+	vals, err = s.repo.List(s.Ctx, rt.ResourceType())
+	require.NoError(s.T(), err)
+
+	require.NotNil(s.T(), vals)
+	require.Len(s.T(), vals, 2)
+}
+
 func (s *resourceTypeScopeBlackBoxTest) TestLookupByResourceTypeAndScope() {
 	rtRef, err := testsupport.CreateTestResourceType(s.Ctx, s.DB, uuid.NewV4().String())
 	require.NoError(s.T(), err)
