0b1f2f54d8dfecf0ce3fd08f39e3bda7afd4de24
Xavier Coulon <xcoulon@redhat.com>
2019-03-12 17:50:12 +0100

    Set default value on all empty `last_activity` records
    Only update null values
    Also make sure that new values do not have an empty value from now on.
    
    Fixes #ODC184
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

---
+++ migration/sql-test-files/046-identity-last-active-default.sql
@@ -0,0 +1,3 @@
+-- insert 2 records
+insert into identities (id, last_active) values ('00000000-0000-0000-0000-000000000001', null);
+insert into identities (id, last_active) values ('00000000-0000-0000-0000-000000000002', '2019-03-11:12:34:56');
