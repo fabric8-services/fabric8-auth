312c2bd8b5c16753093d275ef5c031a066909dea
Xavier Coulon <xcoulon@redhat.com>
2019-04-09 09:34:58 +0200

    increase worker intervals to 4mins
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- configuration/defaults.go
+++ configuration/defaults.go
@@ -123,7 +123,7 @@ vwIDAQAB
 	// defaultPostDeactivationNotificationDelay the default number of milliseconds between to user account deactivation notifications by the same go routine
 	defaultPostDeactivationNotificationDelay = 500
 	// defaultPostDeactivationNotificationDelay the default interval between 2 cycles of the user deactivation worker
-	defaultUserDeactivationWorkerIntervalMinutes = 1
+	defaultUserDeactivationWorkerIntervalMinutes = 4
 	// defaultUserDeactivationNotificationWorkerIntervalMinutes the default interval between 2 cycles of the user deactivation worker
-	defaultUserDeactivationNotificationWorkerIntervalMinutes = 1
+	defaultUserDeactivationNotificationWorkerIntervalMinutes = 4
 )
