6a836eaf516c92312328b7a8b6e94f365136c0f8
nurali-techie <nurali.techie@gmail.com>
2019-04-17 14:40:20 +0530

    minor change: DeactivationNotification check added

--- test/integration/user_deactivation_e2e_test.go
+++ test/integration/user_deactivation_e2e_test.go
@@ -34,6 +34,8 @@ func (s *UserDeactivationSuite) SetupTest() {
 
 func (s *UserDeactivationSuite) Test1() {
 	t := s.T()
+	wantNotificatinCalls := 1
+	wantDeactivateCalls := 1
 	os.Setenv("AUTH_CONFIG_FILE_PATH", "e2e_test_config.yml")
 
 	// start auth_service
@@ -42,12 +44,13 @@ func (s *UserDeactivationSuite) Test1() {
 	err := cmd.Start()
 	assert.NoError(t, err)
 	defer cmd.Process.Kill()
+	log.Println("Auth service started")
 
 	// test data
 	s.createUser()
 
 	// wait and varify
-	timeout := time.NewTimer(30 * time.Second)
+	timeout := time.NewTimer(40 * time.Second)
 	runLoop := true
 	notificatinCalls := 0
 	deactivateCalls := 0
@@ -57,18 +60,20 @@ func (s *UserDeactivationSuite) Test1() {
 			log.Println("Got notification call back")
 			s.UpdateNotificationTime(identityID)
 			notificatinCalls++
+			assert.True(t, (notificatinCalls <= wantNotificatinCalls),
+				"Notification calls exceeds, want:%d, got:%d", wantNotificatinCalls, notificatinCalls)
 		case userID := <-s.DeactivateDone:
 			log.Println("Got deactivate call back")
 			s.VarifyDeactivate(userID)
 			deactivateCalls++
+			assert.True(t, (deactivateCalls <= wantDeactivateCalls),
+				"Deactivation calls exceeds, want:%d, got:%d", wantDeactivateCalls, deactivateCalls)
 		case <-timeout.C:
 			log.Println("Timed out")
 			runLoop = false
 		}
 	}
 
-	wantNotificatinCalls := 1
-	wantDeactivateCalls := 1
 	assert.Equal(t, wantNotificatinCalls, notificatinCalls, "Notification calls not equal, want:%d, got:%d", wantNotificatinCalls, notificatinCalls)
 	assert.Equal(t, wantDeactivateCalls, deactivateCalls, "Deactivation calls not equal, want:%d, got:%d", wantDeactivateCalls, deactivateCalls)
 }
@@ -99,11 +104,31 @@ func (s *UserDeactivationSuite) UpdateNotificationTime(identityID string) {
 	ctx := context.Background()
 	identity, err := s.Application.Identities().Load(ctx, id)
 	require.NoError(t, err)
+	require.NotNil(t, identity)
 
-	// change notificatin time
-	identity.DeactivationNotification = &ago7days
-	err = s.Application.Identities().Save(ctx, identity)
-	require.NoError(t, err)
+	// change notificatin time async
+	go func() {
+		attempt := 5
+		for i := 1; i <= attempt; i++ {
+			identity, err := s.Application.Identities().Load(ctx, id)
+			require.NoError(t, err)
+			require.NotNil(t, identity)
+			if identity.DeactivationNotification != nil {
+				log.Printf("DeactivationNotification will be reset with past dates")
+				identity.DeactivationNotification = &ago7days
+				err = s.Application.Identities().Save(ctx, identity)
+				require.NoError(t, err)
+				return
+			} else {
+				time.Sleep(time.Second)
+			}
+		}
+		identity, err := s.Application.Identities().Load(ctx, id)
+		require.NoError(t, err)
+		require.NotNil(t, identity)
+		require.NotNil(t, identity.DeactivationNotification,
+			"DeactivationNotification is not set after %d seconds from notification call, identity:%v", attempt, identity)
+	}()
 }
 
 func (s *UserDeactivationSuite) VarifyDeactivate(userID string) {
