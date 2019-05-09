376f2b485507632546f97f8b55f374cc63fb88b3
nurali-techie <nurali.techie@gmail.com>
2019-05-09 15:20:35 +0530

    fix TestRunSpaceServiceBlackBoxTest/TestCreateOK

--- migration/sql-files/052-deferrable-constraints2.sql
+++ migration/sql-files/052-deferrable-constraints2.sql
@@ -4,3 +4,6 @@ ALTER table resource_type add constraint resource_type_default_role_id_fkey FORE
 
 ALTER table resource drop constraint resource_parent_resource_id_fkey;
 ALTER table resource add constraint resource_parent_resource_id_fkey FOREIGN KEY (parent_resource_id) REFERENCES resource(resource_id) DEFERRABLE INITIALLY IMMEDIATE;
+
+ALTER table identity_role drop constraint identity_role_resource_id_fkey;
+ALTER table identity_role add constraint identity_role_resource_id_fkey FOREIGN KEY (resource_id) REFERENCES resource(resource_id) DEFERRABLE INITIALLY IMMEDIATE;
