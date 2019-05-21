004b0eb8c5af72fbed5e4f36a03842c072eb2eac
Dipak Pawar <dipakpawar231@gmail.com>
2019-05-21 13:11:42 +0530

    remove push client as FABRIC8_HUB_TOKEN is prompting for password (#852)

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -10,7 +10,8 @@ if [ ! -f .cico-prepare ]; then
 
     run_tests_without_coverage;
 
-    generate_client_setup fabric8-auth auth tool fabric8-services fabric8-auth-client
+    # remove this once FABRIC8_HUB_TOKEN issue is resolved. it's prompting for password
+    #generate_client_setup fabric8-auth auth tool fabric8-services fabric8-auth-client
 
     touch .cico-prepare
 fi
